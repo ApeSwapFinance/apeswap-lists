af88c6a6b6bbb5481d758faec581e357f4bc68b2
Obie Dobo <obiedobo@gmail.com>
2022-09-29 23:27:24 -0400

    chore: SD reward per block update

--- src/constants/jungleFarms.ts
+++ src/constants/jungleFarms.ts
@@ -51,7 +51,7 @@ const jungleFarms: JungleFarmConfig[] = [
     projectLink: 'https://staderlabs.com/',
     twitter: 'https://twitter.com/stader_bnb',
     harvest: true,
-    tokenPerBlock: '0.076607142857142857',
+    tokenPerBlock: '0.05729166666',
     sortOrder: 1,
     isFinished: false,
     tokenDecimals: 18,
