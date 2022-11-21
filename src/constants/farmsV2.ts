b4f59036a0b8859f2f965b06dc38e8a0ad748fad
Harambe-Nakamoto <haramben@protonmail.com>
2022-11-21 11:34:55 -0600

    chore: Add banana farm

--- src/constants/farmsV2.ts
+++ src/constants/farmsV2.ts
@@ -5,6 +5,21 @@ import tokens from './tokens'
 const farmsV2: FarmConfig[] = [
   {
     pid: 0,
+    lpSymbol: tokens.banana.symbol,
+    lpAddresses: {
+      [ChainId.BSC]: tokens.banana.address[ChainId.BSC],
+      [ChainId.BSC_TESTNET]: '',
+    },
+    tokenSymbol: tokens.banana.symbol,
+    tokenAddresses: {
+      [ChainId.BSC]: tokens.banana.address[ChainId.BSC],
+      [ChainId.BSC_TESTNET]: '',
+    },
+    quoteTokenSymbol: QuoteToken.BANANA,
+    quoteTokenAdresses: tokens.banana.address,
+  },
+  {
+    pid: 1,
     lpSymbol: tokens.bananaBusd.symbol,
     lpAddresses: {
       [ChainId.BSC]: tokens.bananaBusd.address[ChainId.BSC],
@@ -19,7 +34,7 @@ const farmsV2: FarmConfig[] = [
     quoteTokenAdresses: tokens.busd.address,
   },
   {
-    pid: 1,
+    pid: 2,
     lpSymbol: tokens.bnbBusd.symbol,
     lpAddresses: {
       [ChainId.BSC]: tokens.bnbBusd.address[ChainId.BSC],
