2f79cc98cd9c99af6a6e3939cd8f6e8e7059773f
Efe <polycrystalefe@gmail.com>
2022-12-06 23:33:07 +0300

    chore: Add GQ Bill

--- src/constants/bills.ts
+++ src/constants/bills.ts
@@ -2,7 +2,7 @@ import { ChainId } from '@ape.swap/sdk'
 import tokens from './tokens'
 import { BillsConfig } from './types'
 
-// BNB Largest ID: 40
+// BNB Largest ID: 41
 // Polygon Largest ID: 103
 // Telos Largest ID: 210
 
@@ -150,6 +150,22 @@ const bills: BillsConfig[] = [
     },
     projectLink: 'https://piston-token.com/',
     twitter: 'https://mobile.twitter.com/pistontoken',
+  },
+  {
+    index: 41,
+    contractAddress: {
+      [ChainId.BSC]: '0x1f61125190b8b52c1c1f33f9958e1989d1a6f8dc',
+    },
+    billType: 'JUNGLE Bill',
+    token: tokens.gq,
+    quoteToken: tokens.busd,
+    lpToken: tokens.gqBusd,
+    earnToken: tokens.gq,
+    billNnftAddress: {
+      [ChainId.BSC]: '0x7FD203888661d1F0AE625eD599909f8F97698670',
+    },
+    projectLink: 'https://outerringmmo.com/',
+    twitter: 'https://twitter.com/OuterRingMMO',
   },
   {
     index: 30,
