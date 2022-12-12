5acd34c703961fc5ccaee9fe8508c9727c8288d3
Harambe-Nakamoto <haramben@protonmail.com>
2022-12-12 13:37:23 -0600

    chore: Add more farms

--- src/constants/farmsV2.ts
+++ src/constants/farmsV2.ts
@@ -48,6 +48,51 @@ const farmsV2: FarmConfig[] = [
     quoteTokenSymbol: QuoteToken.BUSD,
     quoteTokenAdresses: tokens.busd.address,
   },
+  {
+    pid: 3,
+    lpSymbol: tokens.bananaBnb.symbol,
+    lpAddresses: {
+      [ChainId.BSC]: tokens.bananaBnb.address[ChainId.BSC],
+      [ChainId.BSC_TESTNET]: '',
+    },
+    tokenSymbol: tokens.banana.symbol,
+    tokenAddresses: {
+      [ChainId.BSC]: tokens.banana.address[ChainId.BSC],
+      [ChainId.BSC_TESTNET]: '',
+    },
+    quoteTokenSymbol: QuoteToken.BNB,
+    quoteTokenAdresses: tokens.wbnb.address,
+  },
+  {
+    pid: 4,
+    lpSymbol: tokens.bnbBtc.symbol,
+    lpAddresses: {
+      [ChainId.BSC]: tokens.bnbBtc.address[ChainId.BSC],
+      [ChainId.BSC_TESTNET]: '',
+    },
+    tokenSymbol: tokens.btc.symbol,
+    tokenAddresses: {
+      [ChainId.BSC]: tokens.btc.address[ChainId.BSC],
+      [ChainId.BSC_TESTNET]: '',
+    },
+    quoteTokenSymbol: QuoteToken.BNB,
+    quoteTokenAdresses: tokens.wbnb.address,
+  },
+  {
+    pid: 5,
+    lpSymbol: tokens.bnbEth.symbol,
+    lpAddresses: {
+      [ChainId.BSC]: tokens.bnbEth.address[ChainId.BSC],
+      [ChainId.BSC_TESTNET]: '',
+    },
+    tokenSymbol: tokens.eth.symbol,
+    tokenAddresses: {
+      [ChainId.BSC]: tokens.eth.address[ChainId.BSC],
+      [ChainId.BSC_TESTNET]: '',
+    },
+    quoteTokenSymbol: QuoteToken.BNB,
+    quoteTokenAdresses: tokens.wbnb.address,
+  },
 ]
 
 export default farmsV2
