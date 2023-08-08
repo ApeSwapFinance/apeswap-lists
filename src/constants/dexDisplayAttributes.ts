eb14bd8c29ba145fa61a78bf30a22fb421b9dd39
Doublo <doublo54@gmail.com>
2023-08-08 19:00:24 +0200

    feat: added dexDisplayAttributes

---
+++ src/constants/dexDisplayAttributes.ts
@@ -0,0 +1,28 @@
+import { LiquidityDex } from '../types'
+
+export const dexDisplayAttributes: Record<LiquidityDex, { icon: string; tag: string }> = {
+  [LiquidityDex.ApeSwapV2]: {
+    icon: 'https://raw.githubusercontent.com/ApeSwapFinance/apeswap-token-lists/main/assets/APE-LP.svg',
+    tag: 'APESWAP LP',
+  },
+  [LiquidityDex.Algebra]: {
+    icon: 'https://raw.githubusercontent.com/ApeSwapFinance/apeswap-token-lists/main/assets/QUICK.png',
+    tag: 'QUICKSWAP GAMMA LP',
+  },
+  [LiquidityDex.PancakeSwapV2]: {
+    icon: 'https://raw.githubusercontent.com/ApeSwapFinance/apeswap-token-lists/main/assets/CAKE.png',
+    tag: 'PANCAKE LP',
+  },
+  [LiquidityDex.QuickswapV2]: {
+    icon: 'https://raw.githubusercontent.com/ApeSwapFinance/apeswap-token-lists/main/assets/QUICK.png',
+    tag: 'QUICKSWAP LP',
+  },
+  [LiquidityDex.ApeSwapV3]: {
+    icon: '',
+    tag: '',
+  },
+  [LiquidityDex.UniswapV3]: {
+    icon: '',
+    tag: '',
+  },
+}
