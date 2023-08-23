154d79f008e4729f04869d4ec0d74202f6ccd25a
Efe <polycrystalefe@gmail.com>
2023-08-23 13:39:56 +0300

    chore: Add PLT Bill

--- src/constants/bills.ts
+++ src/constants/bills.ts
@@ -2,7 +2,7 @@ import { ChainId } from '@ape.swap/sdk'
 import tokens from './tokens'
 import { BillsConfig, BillVersion } from '../types'
 
-// BNB Largest ID: 82
+// BNB Largest ID: 83
 // Polygon Largest ID: 109
 // Telos Largest ID: 215
 // Arbitrum Largest ID: 2003
@@ -116,6 +116,29 @@ const bills: BillsConfig[] = [
     audit: 'https://paladinsec.co/projects/apeswap/',
     initTime: { [ChainId.BSC]: 1683313200 },
     initPrice: { [ChainId.BSC]: 0.0303088 },
+    soldOut: false,
+  },
+  {
+    index: 83,
+    contractAddress: {
+      [ChainId.BSC]: '0x007eC17bfA484bACcbB6f6bdA20DaD1fB6A49926',
+    },
+    billType: 'liquidity',
+    billVersion: BillVersion.V2,
+    token: tokens.plt,
+    quoteToken: tokens.wbnb,
+    lpToken: tokens.pltBnb_PCS,
+    earnToken: tokens.plt,
+    billNnftAddress: {
+      [ChainId.BSC]: '0xB75BE16984A27d3023e1cF744E2587e9Aa8750c9',
+    },
+    inactive: false,
+    projectLink: 'https://www.poollotto.finance/',
+    twitter: 'https://twitter.com/pltfinance/',
+    audit:
+      'https://github.com/Quillhash/QuillAudit_Reports/blob/master/Poollotto%20Smart%20Contract%20Audit%20Report%20-%20QuillAudits.pdf',
+    initTime: { [ChainId.BSC]: 1692806400 },
+    initPrice: { [ChainId.BSC]: 1.83977 },
     soldOut: false,
   },
   {
