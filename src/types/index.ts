1efddbc5f7fc7da7fece4cdf8e015265be46b5cd
Ape Fede <apefede09@gmail.com>
2023-09-27 21:00:56 +0200

    chore: changed migrationvestingterm type

--- src/types/index.ts
+++ src/types/index.ts
@@ -94,12 +94,14 @@ export enum BillArtCollection {
 
 export const defaultBillArtCollection = BillArtCollection.ApeSwap_Collection1
 
-export enum MigrationVestingTerms {
-  ['60d'] = 5184000,
-  ['120d'] = 10368000,
-  ['180d'] = 15552000,
-  ['360d'] = 31104000,
-}
+export const MigrationVestingTerms = {
+  '60d': 5184000,
+  '120d': 10368000,
+  '180d': 15552000,
+  '360d': 31104000,
+} as const
+
+export type MigrationVestingTermType = typeof MigrationVestingTerms[keyof typeof MigrationVestingTerms]
 
 // Start of list types
 export interface BillsConfig {
