97c15501c217c4d2a4b6475b0f5bf45f3b085370
Efe <polycrystalefe@gmail.com>
2023-12-15 22:52:57 +0300

    chore: Fix bond art for ACF bond

--- src/constants/bills.ts
+++ src/constants/bills.ts
@@ -485,7 +485,7 @@ const bills: BillsConfig[] = [
     initPrice: { [ChainId.BSC]: 0.00543082778284093 },
     soldOut: false,
     billArt: {
-      collection: BillArtCollection.ApeBond_Collection1,
+      collection: BillArtCollection.CexFundBond_Collection1,
     },
   },
   {
