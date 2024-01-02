96400d750c1a0e110450cb8ff5f0013049981e44
n0mbr3al3at0r10 <n0mbr3al3at0r10@gmail.com>
2024-01-02 16:28:36 -0300

    chore: changed CGPT liq dex to PancakeSwapV3

--- src/constants/tokens.ts
+++ src/constants/tokens.ts
@@ -2189,7 +2189,7 @@ const tokens: Record<string, Token> = {
     },
     decimals: { [ChainId.BSC]: 18 },
     liquidityDex: {
-      [ChainId.BSC]: LiquidityDex.PancakeSwapV2,
+      [ChainId.BSC]: LiquidityDex.PancakeSwapV3,
     },
     active: true,
   },
