7a697741b5326cc1756e850a9d0152e8c4106245
Efe <polycrystalefe@gmail.com>
2024-03-12 15:39:01 +0300

    Revert "chore: Add MAINNET WETH token"
    This reverts commit 64ae7761bdb6e6d555c1bc66e5acb667f3b7898d.

--- src/constants/tokens.ts
+++ src/constants/tokens.ts
@@ -95,16 +95,6 @@ const tokens: Record<string, Token> = {
     },
     active: true,
   },
-  weth: {
-    symbol: 'ETH',
-    address: {
-      [ChainId.MAINNET]: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
-    },
-    decimals: {
-      [ChainId.MAINNET]: 18,
-    },
-    active: true,
-  },
   wmatic: {
     symbol: 'MATIC',
     address: {
