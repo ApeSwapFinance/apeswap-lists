18e0b2f7ccfefa05e92ced207a5f75bfef5aa7ff
ape-fede <apefede09@gmail.com>
2024-05-13 10:05:08 -0300

    chore: add abctok token

--- src/constants/tokens.ts
+++ src/constants/tokens.ts
@@ -3,6 +3,16 @@ import { LiquidityDex, Token } from '../types'
 
 const tokens: Record<string, Token> = {
   // BANANA should always be first token for GNANA calculation
+  abctok: {
+    symbol: 'abctok',
+    address: {
+      [ChainId.BSC]: '0x2833412a3ac08Ee81c8590bE0081EB6f5E076BE0',
+    },
+    decimals: {
+      [ChainId.BSC]: 18,
+    },
+    active: true,
+  },
   banana: {
     symbol: 'BANANA',
     address: {
