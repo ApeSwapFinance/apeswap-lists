609fda4a396f6f797039ea8d8ae7bd38a5bdb109
Efe <polycrystalefe@gmail.com>
2024-05-15 20:32:41 +0300

    chore: Add CTOK bond

--- src/constants/bills.ts
+++ src/constants/bills.ts
@@ -5,7 +5,7 @@ import { BillArtCollection, BillsConfig, BillVersion, MigrationVestingTerms } fr
 // BNB Largest ID: 1059
 // Polygon Largest ID: 143
 // Telos Largest ID: 215
-// Arbitrum Largest ID: 2004
+// Arbitrum Largest ID: 2005
 // ETH Largest ID: 3012
 //Migration Bonds ID: 5007
 
@@ -4247,35 +4247,6 @@ const bills: BillsConfig[] = [
     Polygon Bills
   */
   {
-    index: 144,
-    chainId: ChainId.MATIC,
-    contractAddress: {
-      [ChainId.MATIC]: '0xa606Fa38c77A39E36F70D210Ab7b338C6Df6186A',
-    },
-    billType: 'liquidity',
-    billVersion: BillVersion.V2,
-    token: tokens.usdc,
-    quoteToken: tokens.quick,
-    lpToken: tokens.usdcQuick_Ichi,
-    earnToken: tokens.quick,
-    billNnftAddress: {
-      [ChainId.MATIC]: '0x0f59f11AA94e19b796e8a834A89a66F328155181',
-    },
-    inactive: false,
-    projectLink: 'https://ethernity.cloud/',
-    twitter: 'https://twitter.com/Ethernity_cloud',
-    initTime: { [ChainId.MATIC]: 1701892800 },
-    initPrice: { [ChainId.MATIC]: 0.347018667988457 },
-    soldOut: false,
-    billArt: {
-      collection: BillArtCollection.Quickswap_Collection1,
-    },
-    bondPartner: 'QuickSwap',
-    fullDescription:
-      'Ethernity Cloud is a Web3 decentralized confidential computing ecosystem supported by a community of enthusiastic node operators, stakers, and developers. As Web2 solutions fall behind in terms of security and affordability, Ethernity Cloud’s DePin excels with its EVM Confidential Computing architecture. Ethernity Cloud’s bulletproof decentralized architecture and advanced encryption techniques ensure that data remains secure for businesses at all times, whether it is in transit, at rest, or being processed.',
-    tags: ['DEPIN'],
-  },
-  {
     index: 143,
     chainId: ChainId.MATIC,
     contractAddress: {
@@ -5823,6 +5794,34 @@ const bills: BillsConfig[] = [
   /*
       Arbitrum Bills - ID started from 2001
   */
+  {
+    index: 2005,
+    chainId: ChainId.ARBITRUM,
+    contractAddress: {
+      [ChainId.ARBITRUM]: '0x0C61d2E4abCBeF83B5c61739194A26B8ED6A504b',
+    },
+    billType: 'reserve',
+    billVersion: BillVersion.V2,
+    token: tokens.usdt,
+    quoteToken: tokens.usdt,
+    lpToken: tokens.usdt,
+    earnToken: tokens.ctok,
+    billNnftAddress: {
+      [ChainId.ARBITRUM]: '0xAC707999D05C4048844B78c737D9512DFDff524d',
+    },
+    inactive: false,
+    projectLink: 'https://codyfight.com/',
+    twitter: 'https://twitter.com/codyfight',
+    initTime: { [ChainId.ARBITRUM]: 1715789961 },
+    initPrice: { [ChainId.ARBITRUM]: 0.13219279419877278 },
+    soldOut: false,
+    billArt: {
+      collection: BillArtCollection.ApeBond_Collection1,
+    },
+    tags: ['GAMEFI'],
+    fullDescription:
+      "Codyfight's mission is to combine AI, creative freedom, and community innovation through custom games, metagame control, AI battles, and open-source coding sharing. The CTOK token is the heart of Codyfight's economy, facilitating asset exchange and rewarding players.",
+  },
   {
     index: 2004,
     chainId: ChainId.ARBITRUM,
