fa5bd00f56d1c323db1e0cb828f1b46a7588c334
ape-fede <apefede09@gmail.com>
2024-05-21 09:39:36 -0300

    chore: add liquidity dex for fitm

--- src/constants/tokens.ts
+++ src/constants/tokens.ts
@@ -72,6 +72,9 @@ const tokens: Record<string, Token> = {
     decimals: {
       [ChainId.BSC]: 18,
     },
+    liquidityDex: {
+      [ChainId.BSC]: LiquidityDex.PancakeSwapV2,
+    },
     active: true,
   },
   gnana: {
