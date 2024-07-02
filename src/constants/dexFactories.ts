7abd216b59b1e91a27d23bc90c5c317a2595f552
ape-fede <apefede09@gmail.com>
2024-07-02 16:04:40 -0300

    chore: fix typo on router

--- src/constants/dexFactories.ts
+++ src/constants/dexFactories.ts
@@ -134,7 +134,7 @@ export const dexFactories: Partial<
     },
     [LiquidityDex.Aerodrome]: {
       factory: '0x420DD381b31aEf6683db6B902084cB0FFECe40Da',
-      router: '0xcF77a3Ba9A5CA399B7c97c74d54e5b1Beb874E43 ',
+      router: '0xcF77a3Ba9A5CA399B7c97c74d54e5b1Beb874E43',
       protocol: Protocols.Solidly,
     },
     [LiquidityDex.SmarDex]: {
