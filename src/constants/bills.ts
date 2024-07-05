bd82c6694e143cd538838cb03cb52c2fe2540e80
efeDaniels <polycrystalefe@gmail.com>
2024-07-05 18:30:33 +0300

    chore: Push METH bond to soldout tab

--- src/constants/bills.ts
+++ src/constants/bills.ts
@@ -7413,7 +7413,7 @@ const bills: BillsConfig[] = [
     twitter: 'https://twitter.com/Monsta_ETH',
     initTime: { [ChainId.MAINNET]: 1710255600 },
     initPrice: { [ChainId.MAINNET]: 0.00173967992950712 },
-    soldOut: false,
+    soldOut: true,
     billArt: {
       collection: BillArtCollection.ETH_Collection1,
     },
