32fc11b513203cc78b5504a2ed949f62b85457a9
RandomApe <n0mbr3al3at0r10@gmail.com>
2024-08-21 12:18:16 -0300

    Revert "chore: hide vda bond"
    This reverts commit e9bde5ccc2ec50222bd25fe91d35e855afdfd1c1.

--- src/constants/bills.ts
+++ src/constants/bills.ts
@@ -5426,7 +5426,7 @@ const bills: BillsConfig[] = [
     twitter: 'https://x.com/Verida_io',
     audit: 'https://community.verida.network/verida-network/security-audits',
     initTime: { [ChainId.MATIC]: 1723642568 },
-    soldOut: true,
+    soldOut: false,
     billArt: {
       collection: BillArtCollection.ApeBond_Collection1,
     },
