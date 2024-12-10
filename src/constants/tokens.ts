34e816d35ff5069df2947c9a1e484b7198a879e9
efeDaniels <polycrystalefe@gmail.com>
2024-12-10 19:20:12 +0300

    chore: Add IAO & MOZ tokens

--- src/constants/tokens.ts
+++ src/constants/tokens.ts
@@ -302,6 +302,32 @@ const tokens: Record<string, Token> = {
       [ChainId.BASE]: LiquidityDex.UniswapV3,
     },
   },
+  oai: {
+    symbol: 'OAI',
+    address: {
+      [ChainId.BASE]: '0x63c387F45046bc3C4aD8Fe12fe9cfa2A7b305385',
+    },
+    decimals: {
+      [ChainId.BASE]: 18,
+    },
+    active: true,
+    liquidityDex: {
+      [ChainId.BASE]: LiquidityDex.UniswapV3,
+    },
+  },
+  moz: {
+    symbol: 'MOZ',
+    address: {
+      [ChainId.BASE]: '0xC227717ef4Ae4D982E14789eB33bA942243c3FEe',
+    },
+    decimals: {
+      [ChainId.BASE]: 18,
+    },
+    active: true,
+    liquidityDex: {
+      [ChainId.BASE]: LiquidityDex.Aerodrome,
+    },
+  },
   imgnai: {
     symbol: 'imgnAI',
     address: {
