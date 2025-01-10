0d85b01dd00ec07ab32a221af97e737b78ad1945
RandomApe <n0mbr3al3at0r10@gmail.com>
2025-01-10 12:09:14 -0300

    chore: added FDUSD liquidity dex

--- src/constants/tokens.ts
+++ src/constants/tokens.ts
@@ -856,6 +856,9 @@ const tokens: Record<string, Token> = {
     decimals: {
       [ChainId.BSC]: 18,
     },
+    liquidityDex: {
+      [ChainId.BSC]: LiquidityDex.PancakeSwapV3,
+    },
     active: true,
   },
   busd: {
