cf90353b2d36440935ff161b56a82cb890b2d053
efeDaniels <polycrystalefe@gmail.com>
2025-02-06 01:29:24 +0300

    chore: Push eywa to sold out tab

--- src/constants/bills.ts
+++ src/constants/bills.ts
@@ -9802,7 +9802,7 @@ Blockasset’s media arm has amassed over 250 million views, working with top at
     projectLink: 'https://crosscurve.fi/',
     twitter: 'https://x.com/eywaprotocol',
     initTime: { [ChainId.ARBITRUM]: 1737722341 },
-    soldOut: false,
+    soldOut: true,
     billArt: {
       collection: BillArtCollection.ApeBond_Collection1,
     },
