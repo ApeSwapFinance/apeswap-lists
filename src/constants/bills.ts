671e1e7421b0b2795e2d7aac7de4521b2ac1150c
shivammp <pathakshivam983@gmail.com>
2025-03-10 10:52:05 +0530

    chore: update soldout bonds

--- src/constants/bills.ts
+++ src/constants/bills.ts
@@ -10539,7 +10539,7 @@ Blockasset’s media arm has amassed over 250 million views, working with top at
     projectLink: 'https://crosscurve.fi/',
     twitter: 'https://x.com/eywaprotocol',
     initTime: { [ChainId.ARBITRUM]: 1741021108 },
-    soldOut: false,
+    soldOut: true,
     billArt: {
       collection: BillArtCollection.ApeBond_Collection1,
     },
