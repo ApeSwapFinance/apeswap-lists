bf1970a7ee6fac7b975df2d261760871be288c97
efeDaniels <polycrystalefe@gmail.com>
2025-03-25 10:36:33 +0300

    chore: add gold rush linea templates

--- src/constants/bills.ts
+++ src/constants/bills.ts
@@ -1462,6 +1462,63 @@ const bills: BillsConfig[] = [
   //
   // LINEA
   //
+  // {
+  //   index: 6017,
+  //   version: '2.1.0',
+  //   chainId: ChainId.LINEA,
+  //   contractAddress: {
+  //     [ChainId.LINEA]: '',
+  //   },
+  //   billType: 'reserve',
+  //   billVersion: BillVersion.V4,
+  //   lpToken: tokens.usdc,
+  //   earnToken: tokens.hotdog,
+  //   billNnftAddress: {
+  //     [ChainId.LINEA]: '0xFA40b551980aF88FD2929Ab41f57D98ee13A6354',
+  //   },
+  //   inactive: false,
+  //   projectLink: 'https://www.hotdog.pet/',
+  //   twitter: 'https://x.com/HotdogLinea',
+  //   initTime: { [ChainId.LINEA]: 1742922000 },
+  //   soldOut: false,
+  //   billArt: {
+  //     collection: BillArtCollection.GoldenTicket_Collection1,
+  //   },
+  //   tags: ['Meme', 'Community'],
+  //   shortDescription: `HOTDOG, the sauciest meme on Linea, brings a fiery mix of memes, madness, and mouthwatering fun. More than just a token, it’s a cultural sensation powered by 69% memes, 31% hotdogs, and an unstoppable community. With a fresh website, expanding partnerships, and growing momentum, HOTDOG is ready to take a bite out of the market, this is the hottest dog on the blockchain!`,
+  //   fullDescription: `Everyone loves hotdogs. Everyone loves memes. HotDog on Linea is the ultimate snackable sensation—69% memes, 31% hotdogs, 100% delicious. Fresh off the grill, it’s back with a new website, juicier partnerships, and a community that’s always hungry for more. HOTDOG isn’t just a memecoin—it’s a movement, a meme revolution, and the tastiest token on the blockchain. From AI-generated hotdog art to the most unhinged hotdog memes, this is where degens and food lovers unite. Think you can resist? The market’s heating up, and this is your chance to lock in your spot at the meme table. No forks, no knives—just raw, saucy fun. Whether you're here to stack, snack, or just enjoy the absurdity of a blockchain hotdog, this is your moment. Don't miss out—because nothing hits the spot like $HOTDOG!`,
+  //   feeInPayout: 0,
+  //   minTier: LaunchBondTiers.Silver,
+  // },
+  // {
+  //   index: 6016,
+  //   version: '2.1.0',
+  //   chainId: ChainId.LINEA,
+  //   contractAddress: {
+  //     [ChainId.LINEA]: '',
+  //   },
+  //   billType: 'reserve',
+  //   billVersion: BillVersion.V4,
+  //   lpToken: tokens.usdc,
+  //   earnToken: tokens.lynx,
+  //   billNnftAddress: {
+  //     [ChainId.LINEA]: '0xFA40b551980aF88FD2929Ab41f57D98ee13A6354',
+  //   },
+  //   inactive: false,
+  //   projectLink: 'https://www.lynex.fi/',
+  //   twitter: 'https://x.com/LynexFi',
+  //   audit: 'https://www.cyberscope.io/audits/coin-lynex',
+  //   initTime: { [ChainId.LINEA]: 1742922000 },
+  //   soldOut: false,
+  //   billArt: {
+  //     collection: BillArtCollection.GoldenTicket_Collection1,
+  //   },
+  //   tags: ['DEX'],
+  //   fullDescription:
+  //     'Lynex democratizes sophisticated liquidity strategies, seamlessly connecting everyday traders with expert-level capabilities. The platform features a competitive ecosystem of Automated Liquidity Managers (ALMs) and strategists, all striving to optimize returns, minimize risks such as impermanent loss, and boost overall efficiency for every user. Lynex: the Liquidity Engine of Linea!',
+  //   feeInPayout: 0,
+  //   minTier: LaunchBondTiers.Silver,
+  // },
   {
     index: 6015,
     version: '2.0.0',
