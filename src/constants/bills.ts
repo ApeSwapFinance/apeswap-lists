762c86fb774d8477868d6b3f0137f11a21621a79
RandomApe <n0mbr3al3at0r10@gmail.com>
2025-04-07 09:18:35 -0300

    chore: monad bonds soldout

--- src/constants/bills.ts
+++ src/constants/bills.ts
@@ -71,7 +71,7 @@ const bills: BillsConfig[] = [
     inactive: false,
     projectLink: 'https://bitcoin.org/',
     initTime: { [ChainId.MONAD_TESTNET]: 1742905970 },
-    soldOut: false,
+    soldOut: true,
     billArt: {
       collection: BillArtCollection.ApeBond_Collection1,
     },
@@ -99,7 +99,7 @@ const bills: BillsConfig[] = [
     projectLink: 'https://ethereum.org/',
     twitter: 'https://x.com/ethereum',
     initTime: { [ChainId.MONAD_TESTNET]: 1742905970 },
-    soldOut: false,
+    soldOut: true,
     billArt: {
       collection: BillArtCollection.ApeBond_Collection1,
     },
