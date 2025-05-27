220a27ebf165d04ce7d505512cf4e6c3651c2dd6
ape-fede <apefede09@gmail.com>
2025-05-27 10:42:03 -0300

    chore: set new test bonds

--- src/constants/bills.ts
+++ src/constants/bills.ts
@@ -7,14 +7,40 @@ const bills: BillsConfig[] = [
     version: '2.1.1',
     chainId: ChainId.SOL,
     contractAddress: {
-      [ChainId.SOL]: '8KYLiejUgnVLSiPVgUvvbmiPJzxBjBqQjjp6pvZNHw4L', //aka bond issuance PDA
+      [ChainId.SOL]: '4344tkoBWuo5md6ix1FFS5xNvcyZqs13PHACFCekZFB7', //aka bond issuance PDA
     },
     billType: 'reserve',
     billVersion: BillVersion.V2,
     lpToken: tokens.gnana,
     earnToken: tokens.banana,
     billNnftAddress: {
-      [ChainId.SOL]: '7w72f4qNHKUTufxqgk9fSPFYhF6SrNamA6YhbSQakXcG', // aka collection
+      [ChainId.SOL]: '8NhzoQ1r4f2QevbMFWdHBHDbvPtzdq2dyYNL8sszp8no', // aka collection
+    },
+    inactive: false,
+    projectLink: 'https://www.moonbay.space/',
+    twitter: 'https://x.com/MoonBaySonic',
+    soldOut: false,
+    billArt: {
+      collection: BillArtCollection.ApeBond_Collection1,
+    },
+    fullDescription: `I wet my pants thinking in doublo's abs`,
+    shortDescription: `I wet my pants thinking in doublo's abs`,
+    tags: ['SOL'],
+    feeInPayout: 1,
+  },
+  {
+    index: 2,
+    version: '2.1.1',
+    chainId: ChainId.SOL,
+    contractAddress: {
+      [ChainId.SOL]: 'FKV4Zy9AQtKEUr4YKtkU5Y5nKN2Jc69SbAsF7QJ5sMyT', //aka bond issuance PDA
+    },
+    billType: 'reserve',
+    billVersion: BillVersion.V2,
+    lpToken: tokens.gnana,
+    earnToken: tokens.banana,
+    billNnftAddress: {
+      [ChainId.SOL]: 'FK37bJ6HrDZLgmKecJi3KuJbD1eSA2XCbDf5VT6xkPGe', // aka collection
     },
     inactive: false,
     projectLink: 'https://www.moonbay.space/',
