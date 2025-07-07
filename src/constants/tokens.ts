ab6cf9401c97e43faea0c8c95a0f2f4499b32c41
efeDaniels <polycrystalefe@gmail.com>
2025-07-07 21:45:14 +0300

    chore: add bvecatx token

--- src/constants/tokens.ts
+++ src/constants/tokens.ts
@@ -5156,6 +5156,16 @@ const tokens: Record<string, Token> = {
       [ChainId.UNICHAIN]: LiquidityDex.External,
     },
   },
+  bvecatx: {
+    symbol: 'bveCATX',
+    address: {
+      [ChainId.UNICHAIN]: '0x7169a1874d4716094bdcE1b86a5cd00212CA1319',
+    },
+    decimals: {
+      [ChainId.UNICHAIN]: 18,
+    },
+    active: true,
+  },
 
   //***** LIGHTLINK TOKENS  *****/
   ll: {
