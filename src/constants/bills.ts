5fbec9442d9075371d7ad281920613fb9e239dc7
efeDaniels <polycrystalefe@gmail.com>
2025-07-22 16:40:19 +0300

    chore: add galaxis bond

--- src/constants/bills.ts
+++ src/constants/bills.ts
@@ -10243,34 +10243,34 @@ Blockasset’s media arm has amassed over 250 million views, working with top at
   /*
     Polygon Bills
   */
-  // {
-  //   index: 185,
-  //   cmcId: 31154,
-  //   version: '2.1.0',
-  //   chainId: ChainId.MATIC,
-  //   contractAddress: {
-  //     [ChainId.MATIC]: '',
-  //   },
-  //   billType: 'liquidity',
-  //   billVersion: BillVersion.V4,
-  //   lpToken: tokens.usdtGalaxis_Ichi,
-  //   earnToken: tokens.galaxis,
-  //   billNnftAddress: {
-  //     [ChainId.MATIC]: '0x8860ABc05FefEF93306c8d65a5eFD421F155d67E',
-  //   },
-  //   inactive: false,
-  //   projectLink: 'https://galaxis.xyz/#/',
-  //   twitter: 'https://x.com/Galaxisxyz',
-  //   initTime: { [ChainId.MATIC]: 1753101077 },
-  //   soldOut: false,
-  //   billArt: {
-  //     collection: BillArtCollection.ApeBond_Collection1,
-  //   },
-  //   shortDescription:
-  //     'Galaxis is a Web3-powered platform to create dynamic membership card communities. Use $GALAXIS to unlock features, vote, access events, and build a sustainable community economy.',
-  //   fullDescription: `Galaxis empowers creators and communities with customizable, utility-packed membership cards enhanced by badges. From virtual meetings to physical rewards and gamification, Galaxis brings engagement to the next level. At the core of it all is $GALAXIS — the utility token that fuels community growth. Use it to access perks, vote, join events, or stake to boost your treasury and leaderboard rank. Whether you're Web3-native or not, Galaxis helps you build a sustainable and independent community economy.`,
-  //   tags: ['DeFi Tooling'],
-  // },
+  {
+    index: 185,
+    cmcId: 31154,
+    version: '2.1.0',
+    chainId: ChainId.MATIC,
+    contractAddress: {
+      [ChainId.MATIC]: '0x85AC2Ba5051ea2558e68aee2EB9c39BFa9D24DA8',
+    },
+    billType: 'liquidity',
+    billVersion: BillVersion.V4,
+    lpToken: tokens.usdtGalaxis_Ichi,
+    earnToken: tokens.galaxis,
+    billNnftAddress: {
+      [ChainId.MATIC]: '0x8860ABc05FefEF93306c8d65a5eFD421F155d67E',
+    },
+    inactive: false,
+    projectLink: 'https://galaxis.xyz/#/',
+    twitter: 'https://x.com/Galaxisxyz',
+    initTime: { [ChainId.MATIC]: 1753190910 },
+    soldOut: false,
+    billArt: {
+      collection: BillArtCollection.ApeBond_Collection1,
+    },
+    shortDescription:
+      'Galaxis is a Web3-powered platform to create dynamic membership card communities. Use $GALAXIS to unlock features, vote, access events, and build a sustainable community economy.',
+    fullDescription: `Galaxis empowers creators and communities with customizable, utility-packed membership cards enhanced by badges. From virtual meetings to physical rewards and gamification, Galaxis brings engagement to the next level. At the core of it all is $GALAXIS — the utility token that fuels community growth. Use it to access perks, vote, join events, or stake to boost your treasury and leaderboard rank. Whether you're Web3-native or not, Galaxis helps you build a sustainable and independent community economy.`,
+    tags: ['DeFi Tooling'],
+  },
   {
     index: 184,
     cmcId: 29157,
