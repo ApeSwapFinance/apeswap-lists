eb50f8b01b1fb8c94691b32d548c22d35437bac1
efeDaniels <polycrystalefe@gmail.com>
2025-08-13 16:28:34 +0300

    chore: update index

--- src/constants/bills.ts
+++ src/constants/bills.ts
@@ -2,7 +2,7 @@ import tokens from './tokens'
 import { BillArtCollection, BillsConfig, BillVersion, ChainId, LaunchBondTiers, MigrationVestingTerms } from '../types'
 
 // BNB Largest ID: 1163
-// Polygon Largest ID: 184
+// Polygon Largest ID: 187
 // Telos Largest ID: 215
 // Arbitrum Largest ID: 2031
 // ETH Largest ID: 3048
