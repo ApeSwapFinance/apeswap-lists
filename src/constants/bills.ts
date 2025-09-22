2ef5403e61e9c47a3483cd9accc9dd268495078e
shivam <pathakshivam983@gmail.com>
2025-09-22 19:49:52 +0530

    chore: move linus bond to soldout

--- src/constants/bills.ts
+++ src/constants/bills.ts
@@ -3193,7 +3193,7 @@ const bills: BillsConfig[] = [
     projectLink: 'https://linusonlinea.com/',
     twitter: 'https://x.com/linusonlinea',
     initTime: { [ChainId.LINEA]: 1753290000 },
-    soldOut: false,
+    soldOut: true,
     billArt: {
       collection: BillArtCollection.ApeBond_Collection1,
     },
