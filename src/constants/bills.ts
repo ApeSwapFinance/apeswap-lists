20027024b6567d530355c43451a54cd29382519b
RandomApe <n0mbr3al3at0r10@gmail.com>
2025-09-23 09:04:43 -0300

    chore: removed azt monad bond

--- src/constants/bills.ts
+++ src/constants/bills.ts
@@ -997,7 +997,7 @@ const bills: BillsConfig[] = [
     },
     projectLink: 'https://www.azex.io/',
     initTime: { [ChainId.MONAD_TESTNET]: 1746818338 },
-    soldOut: false,
+    soldOut: true,
     billArt: {
       collection: BillArtCollection.ApeBond_Collection1,
     },
