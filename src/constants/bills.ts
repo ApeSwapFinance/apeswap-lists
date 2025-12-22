337f6e70a6f90ba7ca4729d56597c23f7df58641
RandomApe <n0mbr3al3at0r10@gmail.com>
2025-12-22 11:02:29 -0300

    chore: remove wrong bond

--- src/constants/bills.ts
+++ src/constants/bills.ts
@@ -5,7 +5,7 @@ import { BillArtCollection, BillsConfig, BillVersion, ChainId, LaunchBondTiers, 
 // Polygon Largest ID: 188
 // Telos Largest ID: 215
 // Arbitrum Largest ID: 2034
-// ETH Largest ID: 3070
+// ETH Largest ID: 3069
 // Linea Largest ID: 6020
 // Base Largest ID: 8068
 // Lightlink Largest ID: 7003
@@ -18460,36 +18460,6 @@ With the lending products set to go live in February, the project is poised to a
     version: '2.1.0',
     chainId: ChainId.MAINNET,
     contractAddress: {
-      [ChainId.MAINNET]: '0x9B1717ab841622cEB496E3fe0aCBcE9F80feAd49',
-    },
-    billType: 'reserve',
-    billVersion: BillVersion.V4,
-    lpToken: tokens.usdc,
-    earnToken: tokens.pmusd,
-    billNnftAddress: {
-      [ChainId.MAINNET]: '0x499b654b0a7f9979cc315190c09ca2633809b8df',
-    },
-    projectLink: 'https://raac.io',
-    twitter: 'https://x.com/Raacfi',
-    initTime: { [ChainId.MAINNET]: 1765993110 },
-    soldOut: true,
-    hide: false,
-    billArt: {
-      collection: BillArtCollection.ETH_Collection1,
-    },
-    tags: ['RWA'],
-    shortDescription:
-      'RAAC is a decentralized DeFi lending and borrowing protocol powered by tokenized Real World Assets. It enables on-chain borrowing using collateral such as commodities, real estate, and capital market assets, while offering RWA vaults and stability mechanisms. RAAC bridges traditional value with DeFi, with pmUSD acting as its RWA-backed stablecoin.',
-    fullDescription:
-      'RAAC is a decentralized DeFi lending and borrowing ecosystem powered by tokenized Real World Assets, designed to bridge on-chain finance with tangible economic value. The protocol enables users to borrow and lend using RWAs such as commodities, real estate, and capital market instruments as collateral, while also supporting the creation of stablecoins backed by real-world assets. RAAC offers a comprehensive suite of products, including an on-chain lending and borrowing system, RWA vaults, and stability mechanisms that enhance capital efficiency and risk management. Its modular architecture allows seamless integration for external DeFi applications and provides transparent insight into how the protocol is built, governed, and parameterized. By combining decentralized infrastructure with real-world backing, RAAC aims to unlock scalable and sustainable DeFi use cases, with pmUSD serving as its core RWA-backed stablecoin within the ecosystem.',
-    clickUpId: '86adwj35g',
-  },
-  {
-    index: 3070,
-    // cmcId: 0, // TODO: add cmcId when live
-    version: '2.1.0',
-    chainId: ChainId.MAINNET,
-    contractAddress: {
       [ChainId.MAINNET]: '0x3A14d0536A7e3AC545DB5130B2dE0FD840204339',
     },
     billType: 'reserve',
@@ -18513,7 +18483,7 @@ With the lending products set to go live in February, the project is poised to a
     fullDescription:
       'RAAC is a decentralized DeFi lending and borrowing ecosystem powered by tokenized Real World Assets, designed to bridge on-chain finance with tangible economic value. The protocol enables users to borrow and lend using RWAs such as commodities, real estate, and capital market instruments as collateral, while also supporting the creation of stablecoins backed by real-world assets. RAAC offers a comprehensive suite of products, including an on-chain lending and borrowing system, RWA vaults, and stability mechanisms that enhance capital efficiency and risk management. Its modular architecture allows seamless integration for external DeFi applications and provides transparent insight into how the protocol is built, governed, and parameterized. By combining decentralized infrastructure with real-world backing, RAAC aims to unlock scalable and sustainable DeFi use cases, with pmUSD serving as its core RWA-backed stablecoin within the ecosystem.',
     clickUpId: '86adwj35g',
-    minTier: LaunchBondTiers.Gold,
+    // minTier: LaunchBondTiers.Gold, // making this public
     vestingCliff: 5184000,
   },
 ]
