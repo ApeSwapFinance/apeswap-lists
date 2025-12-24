94aca489412c1ef59af424e96d19da226ad16674
Shivam <74736926+shivammp@users.noreply.github.com>
2025-12-24 17:46:10 +0530

    chore: move HTS bond to soldout

--- src/constants/bills.ts
+++ src/constants/bills.ts
@@ -18487,7 +18487,7 @@ With the lending products set to go live in February, the project is poised to a
     projectLink: 'https://www.home3suite.com/',
     twitter: 'https://x.com/Home3Suite',
     initTime: { [ChainId.MAINNET]: 1765267936 },
-    soldOut: false,
+    soldOut: true,
     hide: false,
     billArt: {
       collection: BillArtCollection.ETH_Collection1,
