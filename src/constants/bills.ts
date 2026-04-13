f9ed1b95ca31d63391b805c4ba825929312514f2
efeDaniels <polycrystalefe@gmail.com>
2026-04-13 15:42:02 +0300

    chore: hide ireet bond

--- src/constants/bills.ts
+++ src/constants/bills.ts
@@ -21268,7 +21268,7 @@ With the lending products set to go live in February, the project is poised to a
     twitter: 'https://x.com/Raacfi',
     initTime: { [ChainId.MAINNET]: 1776080801 },
     soldOut: false,
-    hide: false,
+    hide: true,
     billArt: {
       collection: BillArtCollection.ETH_Collection1,
     },
