4381e9ded3fb7dd9f819cbc13bafe33e82047e9d
ape-fede <apefede09@gmail.com>
2026-05-28 13:04:46 -0300

    chore: add sui

--- src/constants/zapTokens.ts
+++ src/constants/zapTokens.ts
@@ -837,6 +837,12 @@ export const zapInputTokens: Partial<Record<ChainId, Token[]>> = {
   ],
   [ChainId.SUI]: [
     {
+      symbol: 'SUI',
+      address: { [ChainId.SUI]: '0x2::sui::SUI' },
+      decimals: { [ChainId.SUI]: 9 },
+      active: true,
+    },
+    {
       symbol: 'USDC',
       address: { [ChainId.SUI]: '0xdba34672e30cb065b1f93e3ab55318768fd6fef66c15942c9f7cb846e2f900e7::usdc::USDC' },
       decimals: { [ChainId.SUI]: 6 },
