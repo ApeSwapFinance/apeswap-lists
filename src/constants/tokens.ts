424662974039412f0c56fecd6004f3ad7778ae90
nombrealeatorio <n0mbr3al3at0r10@gmail.com>
2026-06-12 12:09:07 -0300

    chore: update mvs address

--- src/constants/tokens.ts
+++ src/constants/tokens.ts
@@ -24,7 +24,7 @@ const tokens: Record<string, Token> = {
     // TODO: remove this test token
     symbol: 'MVS',
     address: {
-      [ChainId.BSC]: '0xf5079c2eaf17d793d688b039ea83074b8e888264',
+      [ChainId.BSC]: '0xC4Af00EB52BF9D30cFF628969092912906AF97cd',
     },
     decimals: {
       [ChainId.BSC]: 18,
