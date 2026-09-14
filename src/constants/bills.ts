5cf69397d3a593fc92b42f98e482997d024d9fe2
Shivam <74736926+shivammp@users.noreply.github.com>
2026-09-14 10:02:12 +0530

    chore: hide OABOND bond

--- src/constants/bills.ts
+++ src/constants/bills.ts
@@ -5995,7 +5995,7 @@ const bills: BillsConfig[] = [
     twitter: 'https://x.com/apebond',
     audit: 'https://paladinsec.co/projects/apebond/',
     initTime: { [ChainId.BSC]: 1786378886 },
-    soldOut: false,
+    soldOut: true,
     hide: false,
     billArt: {
       collection: BillArtCollection.ApeBond_Collection1,
